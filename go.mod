b966b1c259cd7c9af4936bd1c7194d58e30a803a
NiviDev <viluronnicolas@gmail.com>
2024-11-01 19:18:40 -0300

    Probando el Wrapper de pokeapi hecho por mtslzr

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module Documentos/ProyectosPersonales/PokedexEnGo/Pokedex/Pokedex
 
 go 1.23.2
+
+require (
+	github.com/mtslzr/pokeapi-go v1.4.0 // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+
+	"github.com/mtslzr/pokeapi-go"
+)
 
 func main() {
 	fmt.Println("Hola mundo!")
+	l, err := pokeapi.Pokemon("bulbasaur")
+	if err != nil {
+		fmt.Println("Error al obtener el pokemon")
+	}
+
+	fmt.Println("Pokemon obtenido: ", l.Name)
 }
