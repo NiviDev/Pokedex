cd1449c6e154cb4e7c405d54f667bb80af8cb061
NiviDev <viluronnicolas@gmail.com>
2025-03-17 21:22:34 -0300

    Ahora muestra el tipo de los pokemon

--- frontend/src/PokemonData.jsx
+++ frontend/src/PokemonData.jsx
@@ -10,10 +10,17 @@ function PokemonData({ pokemon }) {
             </div>
         )
     }
+
+    const typesList = pokemon.types.map(type => 
+        type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1)
+    );
+    const typesText = typesList.length > 1 ? `Types: ${typesList.join(' - ')}` : `Type: ${typesList[0]}`;
+
     return (
         <div id="PokemonData" className="pokemon-data">
             <div id="result" className="result-text">{pokemon.name} #{pokemon.id}</div>
             <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`} alt="pokemon-sprite" width="200" height="200"/>
+            <div id="types" className="data-text">{typesText}</div>
             <div id="weight" className="data-text">Weight: {pokemon.weight} hg</div>
             <div id="height" className="data-text">Height: {pokemon.height} dm</div>
         </div>
